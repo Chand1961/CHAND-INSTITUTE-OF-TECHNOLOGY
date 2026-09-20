f917a4c35a28e119283a6d747102f73de11b6e86
Chand1961 <sefuanzuruni22@gmail.com>
2026-09-20 06:03:08 -0700

    connect supabase

---
+++ js/supabase.js
@@ -0,0 +1,8 @@
+
+const SUPABASE_URL = "https://yylvkfxhmrnlmrqtqfxk.supabase.co/rest/v1/";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_rubHGoRdh_W42eGFmQf9fw_7bkSg1Y7";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+);
